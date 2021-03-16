fc0b0b5ccaa420175c4c558e5bf4a733836dff1b
Akash <aaj7731@gmail.com>
2021-03-16 23:38:18 +0600

    3rd edit from upgrade

--- dj/app/forms.py
+++ dj/app/forms.py
@@ -5,7 +5,16 @@ class UserForm(forms.ModelForm):
     class Meta:
         model=User
         fields=["first_name","last_name","email"]
+        widgets={
+            "first_name":forms.TextInput(attrs={"class":"form-control col-lg-3"}),
+            "last_name":forms.TextInput(attrs={"class":"form-control col-lg-3"}),
+            "email":forms.TextInput(attrs={"class":"form-control col-lg-3"}),
+        }
+        
 class ProForm(forms.ModelForm):
     class Meta:
         model=ProfilUpdate
         fields=["image","bio"]
+        widgets={
+            "bio":forms.Textarea(attrs={"class":"form-control col-lg-3"})
+        }
